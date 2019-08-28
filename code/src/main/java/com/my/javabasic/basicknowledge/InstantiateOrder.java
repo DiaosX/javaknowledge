db8994646c518dc86dc3760835a151b1653a3678
yangshengtao <yangshengtao@yiguo.com>
2019-08-28 09:34:15 +0800

    添加读写锁demo

--- code/src/main/java/com/my/javabasic/basicknowledge/InstantiateOrder.java
+++ code/src/main/java/com/my/javabasic/basicknowledge/InstantiateOrder.java
@@ -3,23 +3,23 @@ package com.my.javabasic.basicknowledge;
 public class InstantiateOrder extends InstantiateOrderParent {
     static {
         InstantiateOrder.executeOrder++;
-        System.out.println(InstantiateOrder.executeOrder + "-子类静态代码块");
+        System.out.println(InstantiateOrder.executeOrder + "-子类静态代码块1");
     }
 
     static {
         InstantiateOrder.executeOrder++;
-        System.out.println(InstantiateOrder.executeOrder + "-子类静态代码块");
+        System.out.println(InstantiateOrder.executeOrder + "-子类静态代码块2");
     }
 
     //代码块
     {
         InstantiateOrder.executeOrder++;
-        System.out.println(InstantiateOrder.executeOrder + "-子类普通代码块");
+        System.out.println(InstantiateOrder.executeOrder + "-子类普通代码块1");
     }
 
     {
         InstantiateOrder.executeOrder++;
-        System.out.println(InstantiateOrder.executeOrder + "-子类普通代码块");
+        System.out.println(InstantiateOrder.executeOrder + "-子类普通代码块2");
     }
 
     public InstantiateOrder() {
---
+++ code/src/main/java/com/my/javabasic/concurrent/lock/readwritelock/MyReadWriteLock.java
@@ -0,0 +1,13 @@
+package com.my.javabasic.concurrent.lock.readwritelock;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class MyReadWriteLock {
+
+    public void doTask() {
+
+        ReentrantReadWriteLock wrLock = new ReentrantReadWriteLock();
+
+    }
+
+}
